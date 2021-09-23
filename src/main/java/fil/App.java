1ec85ac9e2bbb0e15a32a85bb8f9bf581374163d
Gracieux AHLONSOU <ahlonsougracieux@gmail.com>
2021-09-23 16:14:29 +0200

    fichier git

--- src/main/java/fil/App.java
+++ src/main/java/fil/App.java
@@ -1,5 +1,7 @@
+
 package fil;
 
+import java.util.Scanner;
 /**
  * Hello world!
  *
@@ -8,6 +10,31 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scanner = new Scanner(System.in);
+        
+      //  System.out.println( "Hello World!" );
+      //gagne lorsque 4 couleur sont alignés
+        int N = 4;
+        // creation de la grille 8 sur 6
+        int C = 8;
+        int L = 6;
+        char[][] plateau = new char[C][L];
+
+        for (int x = 0; x < C; x++)
+            for (int y = 0; y < L; y++)
+                plateau[x][y] = '.';
+
+        int gagnant = 0;
+
+        // faire un for pour arrêter le jeu pour victoir de J1 ou J2
+        for(int i = 1 ; i <= C*L ; i++) {
+            // afficher la grille :
+            System.out.println("Tour " + i + ", affichage du plateau :");
+
+            for (int loop = 0; loop < C + 2 + 2 * C; loop++)
+                System.out.print('-');
+            System.out.println();
+        }
+    
     }
 }
