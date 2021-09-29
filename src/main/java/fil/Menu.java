58b3e4e05a996908952ebd0070bb4b2533875c16
Karrwolf <maloloyerviaud@gmail.com>
2021-09-29 16:30:13 +0200

    Main Menu

--- src/main/java/fil/Menu.java
+++ src/main/java/fil/Menu.java
@@ -1,26 +1,54 @@
-package fil;
+package Puissance4;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextArea;
 
 public class Menu {
-    public static void main(String[] args) {
-        int a=0;
-        int r;
-        System.out.println("Nouvelle Partie !");
-        System.out.println("Continuer");
-        System.out.println("n");
-        System.out.println("Quittez");
-        System.out.println("entrer le numero correspondant à votre choix");
-        a=Clavier.lireInt();
-        r=menu(a);
-    }
-    static int menu(int x) {
-        switch(x){
-        case 1: System.out.println("vous avez selectionné‚ le choix "+x);break;
-        case 2: System.out.println("vous avez selectionné‚ le choix "+x);break;
-        case 3: System.out.println("vous avez selectionné‚ le choix "+x);break;
-        case 4: System.out.println("vous avez selectionné‚ le choix "+x);break;
-        default:{ System.out.println("vous n'avez selectionné‚ aucun choix ");}
-        }
 
-        return x;
-    }
+	public Menu(final JFrame fenetre) {
+		
+		JMenuBar barreDeMenu = new JMenuBar();
+
+		JMenu menu = new JMenu("Menu");
+
+		JMenuItem menuItemAPropos = new JMenuItem("A propos");
+
+		menuItemAPropos.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFrame fenAPropos = new JFrame();
+				fenAPropos.setTitle("A propos");
+				fenAPropos.setSize(400, 80);
+				fenAPropos.setVisible(true);
+				JTextArea label = new JTextArea(
+						"Bienvenue sur notre puissance 4");
+				fenAPropos.add(label);
+
+			}
+		});
+		
+		JMenuItem menuItemFermer = new JMenuItem("Fermer");
+		menuItemFermer.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				fenetre.dispose();
+			}
+		});
+
+		menu.add(menuItemFermer);
+
+		barreDeMenu.add(menu);
+
+		fenetre.setJMenuBar(barreDeMenu);
+
+		menu.add(menuItemAPropos);
+
+	}
+
 }
