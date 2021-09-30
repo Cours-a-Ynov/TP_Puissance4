28d1217cb8ecf7f5a0a40a9f408c99ef06f367d2
Gracieux AHLONSOU <ahlonsougracieux@gmail.com>
2021-09-30 16:49:57 +0200

    pion et detection des victoires

--- src/main/java/fil/App.java
+++ src/main/java/fil/App.java
@@ -6,8 +6,7 @@ import java.util.Scanner;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
@@ -15,7 +14,7 @@ public class App
       //  System.out.println( "Hello World!" );
       //gagne lorsque 4 couleur sont alignés
         int N = 4;
-        // creation de la grille 8 sur 6
+      // creation de la grille 8 par 6
         int C = 8;
         int L = 6;
         char[][] plateau = new char[C][L];
@@ -26,26 +25,26 @@ public class App
 
         int gagnant = 0;
 
-        // faire un for pour arrêter le jeu pour victoir de J1 ou J2
+        //  un for pour arrêter le jeu pour victoir de J1 ou J2
         for(int i = 1 ; i <= C*L ; i++) {
             // afficher la grille :
             System.out.println("Tour " + i + ", affichage du plateau :");
 
             for (int loop = 0; loop < C + 2 + 2 * C; loop++)
-                System.out.print('-');
+                System.out.print('#');
             System.out.println();
 
             for (int y = 0; y < L; y++) {
-                System.out.print('|');
+                System.out.print('#');
                 for (int x = 0; x < C; x++) {
                     System.out.print(" " + plateau[x][y] + " ");
                 }
-                System.out.print('|');
+                System.out.print('#');
                 System.out.println();
             }
 
             for (int loop = 0; loop < C + 2 + 2 * C; loop++)
-                System.out.print('-');
+                System.out.print('#');
             System.out.println();
 
 
@@ -76,12 +75,111 @@ public class App
                 }
 
             } 
+            int rang = L - 1;
             // placement des pion X & O:   
+            while (plateau[colonne - 1][rang] != '.') {
+                rang--;
+            }
+            plateau[colonne - 1][rang] = (i % 2 == 1 ? 'X' : 'O');
+
+            // Détection de victoire:
+            			
+			//symbole en cours:
+			char symbole = (i%2==1 ? 'X' : 'O');
+
+			//nombre alignés maximal:
+			int max = 0;
+			int x; int y;
+            int somme;
 
 
-          //  System.out.println("Tour du joueur " + (i % 2 == 1 ? 'X' : 'O'));
+            // --> diagonale HG-BD
+            x = colonne - 1;
+            y = rang;
+            somme = -1;
+            while (y >= 0 && x >= 0 && plateau[x][y] == symbole) {
+                y--;
+                x--;
+                somme++;
+            }
+            x = colonne - 1;
+            y = rang;
+            while (y < L && x < C && plateau[x][y] == symbole) {
+                y++;
+                x++;
+                somme++;
+            }
+            if (somme > max)
+                max = somme;
 
+            // --> diagonale HD-BG
+            x = colonne - 1;
+            y = rang;
+            somme = -1;
+            while (y >= 0 && x < C && plateau[x][y] == symbole) {
+                y--;
+                x++;
+                somme++;
+            }
+            x = colonne - 1;
+            y = rang;
+            while (y < L && x >= 0 && plateau[x][y] == symbole) {
+                y++;
+                x--;
+                somme++;
+            }
+            if (somme > max)
+                max = somme;
+
+            // --> verticale:
+            x = colonne - 1;
+            y = rang;
+            somme = -1;
+            while (y >= 0 && plateau[x][y] == symbole) {
+                y--;
+                somme++;
+            }
+            y = rang;
+            while (y < L && plateau[x][y] == symbole) {
+                y++;
+                somme++;
+            }
+            if (somme > max)
+                max = somme;
+
+            // --> horizontale:
+            x = colonne - 1;
+            y = rang;
+            somme = -1;
+            while (x >= 0 && plateau[x][y] == symbole) {
+                x--;
+                somme++;
+            }
+            x = colonne - 1;
+            while (x < C && plateau[x][y] == symbole) {
+                x++;
+                somme++;
+            }
+            if (somme > max)
+                max = somme;
+
+            if (max >= N) {
+                gagnant = (i % 2 == 1 ? 1 : 2);
+                i = C * L + 1;
+            }
+
+            System.out.println("*************************");
         }
-    
-    }
+		
+	}
+
 }
+
+
+        
+    
+    
+
+
+    
+
